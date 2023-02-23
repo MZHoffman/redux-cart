88190774a7c56c90b4cf4b47c4299acf323de545
Michal Hoffman <michal.z.hoffman@gmail.com>
2023-02-23 15:21:42 +0000

    updating number of items in cart button

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,9 +1,11 @@
 import classes from './CartButton.module.css'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { cartActions } from '../../store'
 
 const CartButton = (props) => {
   const dispatch = useDispatch()
+  const numberOfItems = useSelector((state) => state.cartItem.items.length)
+  console.log(numberOfItems)
 
   const cartToggleHandler = () => {
     dispatch(cartActions.toggleCart())
@@ -12,7 +14,7 @@ const CartButton = (props) => {
   return (
     <button className={classes.button} onClick={cartToggleHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{numberOfItems}</span>
     </button>
   )
 }

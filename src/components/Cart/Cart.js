7ec2f3fbd6b4e114f2c00d35fd30c2ac3727c187
Michal Hoffman <michal.z.hoffman@gmail.com>
2023-02-23 15:10:06 +0000

    generating cart items from state

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,18 +1,31 @@
-import Card from '../UI/Card';
-import classes from './Cart.module.css';
-import CartItem from './CartItem';
+import { useSelector } from 'react-redux'
+
+import Card from '../UI/Card'
+import classes from './Cart.module.css'
+import CartItem from './CartItem'
 
 const Cart = (props) => {
+  const cartItems = useSelector((state) => state.cartItem)
+  console.log(cartItems)
+
   return (
     <Card className={classes.cart}>
       <h2>Your Shopping Cart</h2>
       <ul>
-        <CartItem
-          item={{ title: 'Test Item', quantity: 3, total: 18, price: 6 }}
-        />
+        {cartItems.items.map((item) => (
+          <CartItem
+            key={item.name}
+            item={{
+              title: item.title,
+              quantity: item.quantity,
+              total: 18,
+              price: item.price,
+            }}
+          />
+        ))}
       </ul>
     </Card>
-  );
-};
+  )
+}
 
-export default Cart;
+export default Cart

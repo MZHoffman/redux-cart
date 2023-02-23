42c55722adede3ee565527888fd62456efdebf27
Michal Hoffman <michal.z.hoffman@gmail.com>
2023-02-23 15:12:55 +0000

    adding to cart from product list

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,11 +1,27 @@
+import { useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
+import { cartItemsActions } from '../../store'
+
 import Card from '../UI/Card'
 import classes from './ProductItem.module.css'
 
 const ProductItem = (props) => {
+  const dispatch = useDispatch()
+  const cartItems = useSelector((state) => state.cartItem)
+
   const { title, price, description } = props
 
+  const indexOfIteam = cartItems.items.findIndex((item) => item.title === title)
   const addHandler = () => {
-    console.log({ title, price })
+    // console.log(indexOfIteam >= 0)
+    if (indexOfIteam >= 0) {
+      // console.log(cartItems.items[indexOfIteam].quantity + 1)
+      // console.log(cartItems.items)
+      console.log(cartItems)
+      dispatch(cartItemsActions.incrementItem(indexOfIteam))
+      return
+    }
+    dispatch(cartItemsActions.addItem({ title, price, quantity: 1, total: 18 }))
   }
 
   return (

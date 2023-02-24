74dcedb270ff6207a50165050e8b34f2b75a2fd5
Michal Hoffman <michal.z.hoffman@gmail.com>
2023-02-24 12:50:05 +0000

    removing redundant code

--- src/store/index.js
+++ src/store/index.js
@@ -2,33 +2,11 @@ import { createSlice, configureStore } from '@reduxjs/toolkit'
 import uiSlice from './ui-slice'
 import cartSlice from './cart-slice'
 
-const cartItemsSlice = createSlice({
-  name: 'cartItems',
-  initialState: {
-    items: [],
-  },
-  reducers: {
-    addItem(state, action) {
-      state.items.push(action.payload)
-    },
-    incrementItem(state, action) {
-      state.items[action.payload].quantity++
-    },
-    removeItem(state, action) {
-      state.items.splice(action.payload, 1)
-    },
-    decrementItem(state, action) {
-      state.items[action.payload].quantity--
-    },
-  },
-})
 const store = configureStore({
   reducer: {
     ui: uiSlice.reducer,
     cart: cartSlice.reducer,
-    // cartItem: cartItemsSlice.reducer,
   },
 })
-// export const cartItemsActions = cartItemsSlice.actions
 
 export default store

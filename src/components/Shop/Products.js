86953724817f69dabca18dae5c14bcdfd1fb8ef1
Michal Hoffman <michal.z.hoffman@gmail.com>
2023-02-24 13:09:26 +0000

    Getting products from an array of objects

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,19 +1,33 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from './ProductItem'
+import classes from './Products.module.css'
+
+const DUMMY_PRODUCTS = [
+  { id: 'i1', title: 'Book', price: 6, description: 'A really cool book!' },
+  {
+    id: 'i2',
+    title: 'New Book',
+    price: 7,
+    description: 'An event cooler book!',
+  },
+]
 
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map((item) => (
+          <ProductItem
+            key={item.id}
+            id={item.id}
+            title={item.title}
+            price={item.price}
+            description={item.description}
+          />
+        ))}
       </ul>
     </section>
-  );
-};
+  )
+}
 
-export default Products;
+export default Products

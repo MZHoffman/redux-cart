05da677dd5cbabdf9f9b5fd8012d8f1168e21c39
Michal Hoffman <michal.z.hoffman@gmail.com>
2023-02-28 11:10:01 +0000

    moving http request to action creator

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { useSelector, useDispatch } from 'react-redux'
 import { useEffect, Fragment } from 'react'
-
-import { uiActions } from './store/ui-slice'
+import { sendCartData } from './store/cart-slice'
 
 import Cart from './components/Cart/Cart'
 import Layout from './components/Layout/Layout'
@@ -17,42 +16,11 @@ function App() {
   const notification = useSelector((state) => state.ui.notification)
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: 'pending',
-          title: 'Sending..',
-          message: 'Sending cart data!!!',
-        })
-      )
-      const response = await fetch(
-        'https://redux-cart-438b2-default-rtdb.europe-west1.firebasedatabase.app/cart.json',
-        { method: 'PUT', body: JSON.stringify(cart) }
-      )
-      if (!response.ok) {
-        throw new Error('Saending cart failed!')
-      }
-      dispatch(
-        uiActions.showNotification({
-          status: 'success',
-          title: 'Success!!!',
-          message: 'Sent cart data successfully!',
-        })
-      )
-    }
     if (isInitial) {
       isInitial = false
       return
     }
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: 'error',
-          title: 'Error!!!',
-          message: 'Sent cart data failed!',
-        })
-      )
-    })
+    dispatch(sendCartData(cart))
   }, [cart, dispatch])
 
   return (
